dbc1796cd9a63daf14c8fbf55e7b80f863d2b36c
burtsev <burtsev.m@gmail.com>
2023-01-26 14:05:47 +0300

    img2prmpt change image type to default (RGB)

--- modules/ui.py
+++ modules/ui.py
@@ -222,7 +222,7 @@ def interrogate_craitvt(image, craitvt_gallery, index):
     print('Selected image info: ', craitvt_gallery[index])
     print('Selected image name: ', craitvt_gallery[index]['name'])
     # img from file
-    image = gr.Image(value = craitvt_gallery[index]['name'], show_label=False, source="upload", type="pil", image_mode="RGBA")
+    image = gr.Image(value = craitvt_gallery[index]['name'], show_label=False, source="upload", type="pil")
     prompt = shared.interrogator.interrogate(image)
     print('img2prmpt: ', prompt)
     print(':-E interrogator finished')
